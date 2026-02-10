6575daf9caaa7c0e58b6bfe6e0624ec4dcde2a71
Albasoft-tech <albasoft.tech@gmail.com>
2026-02-10 05:08:45 +0100

    fixed cors

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
   cors({
     origin: ALLOWED_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
