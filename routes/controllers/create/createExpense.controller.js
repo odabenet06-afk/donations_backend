b774c7b46ebb076b9157a08f555857f6ec8bf57f
Albasoft-tech <albasoft.tech@gmail.com>
2026-02-11 03:15:22 +0100

    bug

--- routes/controllers/create/createExpense.controller.js
+++ routes/controllers/create/createExpense.controller.js
@@ -1,6 +1,8 @@
-export async function createExpenseController(req, res) {
+import handleTokens from "../../adminFunctions/handleTokens";
+import { createExpense } from "../../models/create/createExpense.model";
 
-  const { expenseData } = req.body; 
+export async function createExpenseController(req, res) {
+  const { expenseData } = req.body;
 
   const authHeader = req.headers["authorization"];
   if (!authHeader) return res.status(401).json({ message: "Missing token" });
@@ -13,14 +15,14 @@ export async function createExpenseController(req, res) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
-
   const dbResult = await createExpense(expenseData, decoded.username);
 
   if (dbResult.success) {
+    if (typeof toggleReload === "function") toggleReload();
 
-    if (typeof toggleReload === 'function') toggleReload(); 
-    
-    return res.status(201).json({ message: "Expense created", id: dbResult.id });
+    return res
+      .status(201)
+      .json({ message: "Expense created", id: dbResult.id });
   }
 
   return res.status(500).json({ error: dbResult.error });
